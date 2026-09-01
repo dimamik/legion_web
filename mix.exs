21f3058f46f7a6c7953f329e1171c7400cbdc688
Dima Mikielewicz <dymitrmikielewicz@gmail.com>
2026-09-01 17:43:03 +0200

    Bump deps

--- mix.exs
+++ mix.exs
@@ -60,8 +60,7 @@ defmodule LegionWeb.MixProject do
 
   defp deps do
     [
-      {:legion, git: "https://github.com/software-mansion-labs/legion.git"},
-      # {:legion, "~> 0.5"},
+      {:legion, "~> 0.5"},
       {:phoenix, "~> 1.7"},
       {:phoenix_html, "~> 4.0"},
       {:phoenix_live_view, "~> 1.0"},
